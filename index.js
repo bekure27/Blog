c1b291db17548b63887de2dadcff852acf6858e5
bekure27 <bekure27@gmail.com>
2023-02-02 09:14:32 +0300

    add blog templete

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const http = require('http')
-const server = http.createServer((req, res) =>{
-if(req.url === '/about')
-res.end('The about page')
-else if(req.url === '/contact')
-res.end('The contact page')
-else if(req.url === '/')
-res.end('The home page')
-else {
-res.writeHead(404)
-res.end('page not found')
-}
+const express = require('express');
+const app = new express();
+app.use(express.static('public'));
+app.listen(4000,()=>{
+    console.log('listening on port 4000');
 })
-server.listen(3000)
