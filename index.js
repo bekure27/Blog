77b7375243da2a7241281bcb593835125cc7aebe
bekure27 <bekure27@gmail.com>
2023-02-03 16:09:50 +0300

    add new post page

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = new express();
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 app.set('view engine','ejs');
 app.use(express.static('public'));
-
+mongoose.connect('mongodb://localhost/my_database',{useNewUrlParser:true});
 app.get('/',(req,res)=> {
     res.render('index');
 });
@@ -20,6 +22,10 @@ app.get('/post', (req,res)=> {
 app.get('/contact', (req,res)=> {
     res.render('post')
 });
+app.get('/posts/new',(req,res)=> {
+    res.render('create');
+})
+
 app.listen(4000,()=>{
     console.log('listening on port 4000');
-})
+});
